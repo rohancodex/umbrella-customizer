304fa297a49a9f8cf216639b3d21d5bd44833f38
rohancodex <77398907+rohancodex@users.noreply.github.com>
2022-06-08 12:14:30 +0530

    favicon added

--- src/components/VariantButton.jsx
+++ src/components/VariantButton.jsx
@@ -3,29 +3,28 @@ const VariantButton = (props) => {
     var style = getComputedStyle(document.body);
     //identify who clicked
     if (e.target.classList.contains("blue-btn")) {
+      props.setimg(props.blue);
       //change background color and upload btn color
       document.body.style.background = style.getPropertyValue("--light-blue");
       document.getElementById("upload-btn").style.backgroundColor =
         style.getPropertyValue("--blue");
-
-      props.setimg(props.blue);
     }
 
     if (e.target.classList.contains("pink-btn")) {
+      props.setimg(props.pink);
       //change background color and upload btn color
       document.body.style.backgroundColor =
         style.getPropertyValue("--light-pink");
       document.getElementById("upload-btn").style.backgroundColor =
         style.getPropertyValue("--pink");
-      props.setimg(props.pink);
     }
 
     if (e.target.classList.contains("yellow-btn")) {
+      props.setimg(props.yellow);
       //change background color and upload btn color
       document.body.style.background = "lightyellow";
       document.getElementById("upload-btn").style.backgroundColor =
         style.getPropertyValue("--yellow");
-      props.setimg(props.yellow);
     }
   }
   return (
